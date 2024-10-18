29c9cff6a3d5e056f5bffdd3e164b25c9fcaf1da
apurvwaghmare <apurvawaghamare1234@gmail.com>
2024-10-18 17:46:22 +0530

    updated app.js

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
 const express = require('express');
-const mysql = require('mysql2/promise');
+const mysql = require('mysql2');
 const bcrypt = require('bcrypt');
 const path = require('path');
 const session = require('express-session');
