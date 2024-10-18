a4556543d457de6b4947e8ec99dc454d72a463c7
apurvwaghmare <apurvawaghamare1234@gmail.com>
2024-10-18 16:44:11 +0530

    mysql connection

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
 const express = require('express');
-const mysql = require('mysql2');
+const mysql = require('mysql2/promise');
 const bcrypt = require('bcrypt');
 const path = require('path');
 const session = require('express-session');
