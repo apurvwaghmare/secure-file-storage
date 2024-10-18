30d1f0f6c40bf189ae863e4953c9fdc7aa7234d0
apurvwaghmare <apurvawaghamare1234@gmail.com>
2024-10-18 23:29:22 +0530

    updated blowfish.js for binary data

--- backend/blowfish.js
+++ backend/blowfish.js
@@ -2,40 +2,39 @@ const CryptoJS = require('crypto-js');
 const fs = require('fs');
 
 const encryptFile = (inputFile, outputFile, key) => {
-  return new Promise((resolve, reject) => {
-    fs.readFile(inputFile, (err, data) => {
-      if (err) return reject(err);
-
-      const encrypted = CryptoJS.Blowfish.encrypt(data.toString('binary'), key).toString();
-
-      fs.writeFile(outputFile, encrypted, (err) => {
-        if (err) return reject(err);
-        resolve();
-      });
+    return new Promise((resolve, reject) => {
+        fs.readFile(inputFile, (err, data) => {
+            if (err) return reject(err);
+            const wordArray = CryptoJS.lib.WordArray.create(data);
+            const encrypted = CryptoJS.Blowfish.encrypt(wordArray, key).toString();
+            fs.writeFile(outputFile, encrypted, 'utf8', (err) => {
+                if (err) return reject(err);
+                resolve();
+            });
+        });
     });
-  });
 };
 
 const decryptFile = (inputFile, outputFile, key) => {
-  return new Promise((resolve, reject) => {
-      fs.readFile(inputFile, (err, data) => {
-          if (err) return reject(err);
-          try {
-              const decrypted = CryptoJS.Blowfish.decrypt(data.toString(), key).toString(CryptoJS.enc.Utf8);
-              fs.writeFile(outputFile, decrypted, { encoding: 'binary' }, (err) => {
-                  if (err) return reject(err);
-                  resolve();
-              });
-          } catch (error) {
-              reject(error);
-          }
-      });
-  });
+    return new Promise((resolve, reject) => {
+        fs.readFile(inputFile, 'utf8', (err, encryptedData) => {
+            if (err) return reject(err);
+            try {
+                const decrypted = CryptoJS.Blowfish.decrypt(encryptedData, key);
+                const binaryData = Buffer.from(decrypted.toString(CryptoJS.enc.Latin1), 'binary');
+                fs.writeFile(outputFile, binaryData, (err) => {
+                    if (err) return reject(err);
+                    resolve();
+                });
+            } catch (error) {
+                reject(error);
+            }
+        });
+    });
 };
 
-
 const generateKey = () => {
-  return CryptoJS.lib.WordArray.random(16).toString(); 
+    return CryptoJS.lib.WordArray.random(16).toString();
 };
 
 module.exports = { encryptFile, decryptFile, generateKey };
