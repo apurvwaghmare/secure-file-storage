048e3e851b56863332a4176ca8d1651acd0a75ea
apurvwaghmare <apurvawaghamare1234@gmail.com>
2024-10-23 10:37:17 +0530

    updated blowfish.js

--- backend/blowfish.js
+++ backend/blowfish.js
@@ -1,36 +1,35 @@
 const CryptoJS = require('crypto-js');
 const fs = require('fs');
+const { Transform } = require('stream');
 
 const encryptFile = (inputFile, outputFile, key) => {
     return new Promise((resolve, reject) => {
-        const readStream = fs.createReadStream(inputFile);
-        let encryptedData = '';
+        const readStream = fs.createReadStream(inputFile, { encoding: 'binary' });
+        const writeStream = fs.createWriteStream(outputFile, { encoding: 'binary' });
 
-        readStream.on('data', (chunk) => {
-            const wordArray = CryptoJS.lib.WordArray.create(chunk);
-            const encrypted = CryptoJS.Blowfish.encrypt(wordArray, key).toString();
-            encryptedData += encrypted;
+        const transform = new Transform({
+            transform(chunk, encoding, callback) {
+                const wordArray = CryptoJS.lib.WordArray.create(Buffer.from(chunk, 'binary'));
+                const encrypted = CryptoJS.Blowfish.encrypt(wordArray, key).toString();
+                this.push(encrypted);
+                callback();
+            }
         });
 
-        readStream.on('end', () => {
-            fs.writeFile(outputFile, encryptedData, 'utf8', (err) => {
-                if (err) return reject(err);
-                resolve();
-            });
-        });
-
-        readStream.on('error', reject);
+        readStream.pipe(transform).pipe(writeStream)
+            .on('finish', resolve)
+            .on('error', reject);
     });
 };
 
 const decryptFile = (inputFile, outputFile, key) => {
     return new Promise((resolve, reject) => {
-        fs.readFile(inputFile, 'utf8', (err, encryptedData) => {
+        fs.readFile(inputFile, { encoding: 'binary' }, (err, encryptedData) => {
             if (err) return reject(err);
             try {
                 const decrypted = CryptoJS.Blowfish.decrypt(encryptedData, key);
                 const binaryData = Buffer.from(decrypted.toString(CryptoJS.enc.Latin1), 'binary');
-                fs.writeFile(outputFile, binaryData, (err) => {
+                fs.writeFile(outputFile, binaryData, { encoding: 'binary' }, (err) => {
                     if (err) return reject(err);
                     resolve();
                 });
